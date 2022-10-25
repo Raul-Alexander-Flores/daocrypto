cb9cfb13d5a22e8e143f7e3889514d969a513793
Raul Flores <raul.alexander.flores1@gmail.com>
2022-10-24 22:59:45 -0700

    file structure

---
+++ next/hardhat/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xd5417e77cA983CF11bbfc6EA42352F1D7e9A6511";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
